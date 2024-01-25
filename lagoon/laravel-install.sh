7407143cb424d0db202d53a50981e9646c1cc12a
Bryan Gruneberg <hello@bryangruneberg.com>
2024-01-24 22:24:20 -0600

    Lagoon Scaffolding

--- lagoon/laravel-install.sh
+++ lagoon/laravel-install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo "Laravel install starting"
 if [ ! -f "/app/_tmp-laravel/composer.json" ]; then
   composer create-project laravel/laravel _tmp-laravel
 fi
 
 cd _tmp-laravel
 
+echo "Syncing new Laravel"
 rsync -a \
   --exclude ".git" \
   --exclude ".gitignore" \
@@ -13,3 +15,6 @@ rsync -a \
   --exclude "README.md" \
   . /app
 #  --dry-run \
+
+echo "Laravel synced to the app directory"
+echo "Laravel install complete"
