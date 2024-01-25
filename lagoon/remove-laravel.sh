422ea11fb6bcf255de0a653f946f35687274a1c5
Bryan Gruneberg <hello@bryangruneberg.com>
2024-01-24 21:51:25 -0600

    Lagoon Scaffolding

---
+++ lagoon/remove-laravel.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rf .editorconfig
+rm -rf .gitattributes
+rm -rf app/
+rm -rf artisan
+rm -rf bootstrap/
+rm -rf composer.json
+rm -rf composer.lock
+rm -rf config/
+rm -rf database/
+rm -rf package.json
+rm -rf phpunit.xml
+rm -rf public/
+rm -rf resources/
+rm -rf routes/
+rm -rf tests/
+rm -rf vendor/
+rm -rf vendor/
+rm -rf vite.config.js
